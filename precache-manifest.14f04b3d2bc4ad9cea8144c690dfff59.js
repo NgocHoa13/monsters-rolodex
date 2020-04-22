5971b4fc7b25385df8212d6b29a71716d639b092
ngochoa <nguyenngochoa3979@gmail.com>
2020-04-23 00:56:14 +0700

    Updates

---
+++ precache-manifest.14f04b3d2bc4ad9cea8144c690dfff59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "261464085f0468e8586726c425ec0193",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "851d518a8aa9430515c7",
+    "url": "/monsters-rolodex/static/css/main.9029f6a8.chunk.css"
+  },
+  {
+    "revision": "f3bc58ad93c95ac2dfd4",
+    "url": "/monsters-rolodex/static/js/2.7158d2bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7158d2bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "851d518a8aa9430515c7",
+    "url": "/monsters-rolodex/static/js/main.6caa5983.chunk.js"
+  },
+  {
+    "revision": "5186047b825f1e97286f",
+    "url": "/monsters-rolodex/static/js/runtime-main.34ec292c.js"
+  }
+]);
